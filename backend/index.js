4050245871c063ca5b76282fedbd2fa443094174
jawdonchan <118231622+jawdonchan@users.noreply.github.com>
2023-10-18 16:52:58 +0800

    set up mysql

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express"
+import mysql from "mysql"
+// import cors from "cors"
+ 
+const app = express()
+
+const db = mysql.createConnection({
+    host:"127.0.0.1",
+    user:"root",
+    password:"",
+    database: "crud",
+    port: "3303"
+
+})
+
+// if there is an auth problem
+// alter user 'root'@'localhost' identified with mysql_native_password by '';
+
+app.use(express.json())
+// app.use(cors())
+
+app.get("/", (req,res)=>{
+    res.json("hello this is the backend")
+})
+
+app.get("/seat", (req,res)=>{
+    const q = "SELECT * from seating"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/seat", (req,res)=>{
+    const q = "INSERT INTO seats (`seatcol`,`year`) values (?)"
+    const values = [req.body.seatcol, req.body.year]
+
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Seats has been added!")
+    })
+})
+
+app.listen(8800, ()=>{
+    console.log("connected to crud app!")
+})
+

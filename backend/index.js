ad016f29c9d2fd17ca86c0bce5f56918fcc46f07
jawdonchan <118231622+jawdonchan@users.noreply.github.com>
2023-10-25 13:38:05 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -144,22 +144,52 @@ app.post("/api/login", (req, res) => {
   });
   
 //qr inserting 2 tables
+// app.post('/insertStudent', (req, res) => {
+//     const { adminNo } = req.body;
+   
+//     //checking if adminNo starts with '21'
+    
+
+//     // Define the SQL query to insert a new student's admin number
+//     const insertQuery = 'INSERT INTO display (student) VALUES (?)';
+  
+//     const values = [adminNo];
+  
+//     db.query(insertQuery, values, (err, data) => {
+//       if (err) {
+//         console.error('Error inserting student:', err);
+//         return res.status(500).json({ error: 'Internal server error' });
+//       }
+//       return res.json('Student has been inserted!');
+//     });
+//   });
+
 app.post('/insertStudent', (req, res) => {
     const { adminNo } = req.body;
-  
-    // Define the SQL query to insert a new student's admin number
-    const insertQuery = 'INSERT INTO display (student) VALUES (?)';
-  
-    const values = [adminNo];
-  
+    const firstTwoDigits = adminNo.slice(1, 3);
+    // Check the value of the student column added
+    let year = '';
+    if (firstTwoDigits === '21') {
+        year = 'year 1';
+    } else if (firstTwoDigits === '22') {
+        year = 'year 2';
+    } else if (firstTwoDigits === '23') {
+        year = 'year 3';
+    }
+
+    // Define the SQL query to insert a new student's admin number and year
+    const insertQuery = 'INSERT INTO display (student, year) VALUES (?, ?)';
+
+    const values = [adminNo, year];
+
     db.query(insertQuery, values, (err, data) => {
-      if (err) {
-        console.error('Error inserting student:', err);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-      return res.json('Student has been inserted!');
+        if (err) {
+            console.error('Error inserting student:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        return res.json('Student has been inserted!');
     });
-  });
+});
   
   app.post('/insertSeatcol', (req, res) => {
     const { adminNo } = req.body;

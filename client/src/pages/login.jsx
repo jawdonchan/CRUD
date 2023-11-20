3a41dfd7bd880076859e6d4a304110f15a875d95
jawdonchan <118231622+jawdonchan@users.noreply.github.com>
2023-11-20 16:29:57 +0800

    login animation

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -5,11 +5,14 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from "react-router-dom";
+import "../css/styles.css"; // Import the CSS file
+import '../css/login.css';
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Track login state
   const navigate = useNavigate();
 
   const handleGuestClick = () => {
@@ -18,22 +21,28 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8800/api/login", { username, password });
+      const response = await axios.post("http://localhost:8800/api/login", {
+        username,
+        password,
+      });
       setMessage(response.data.message);
-  
-      // Check if the response includes user information
+
       if (response.data.user) {
         const { username, role } = response.data.user;
-  
-        // Store username and role in sessionStorage
+
         sessionStorage.setItem("username", username);
         sessionStorage.setItem("role", role);
-  
+
         console.log("Stored username:", sessionStorage.getItem("username"));
         console.log("Stored role:", sessionStorage.getItem("role"));
+
+        // Set login state to trigger animation
+        setIsLoggedIn(true);
       }
-  
-      navigate('/events'); // Use navigate for programmatic navigation
+
+      setTimeout(() => {
+        navigate("/events");
+      }, 1000); // Navigate after the animation
     } catch (error) {
       setMessage("Login failed. Check your credentials.");
     }
@@ -41,45 +50,40 @@ function Login() {
   
 
   return (
-    <div className="App">
-       <Stack  spacing={5} alignItems="center">
-        <br></br>
-         <Typography variant="h4">Login Page</Typography>
-      <br></br>
-      <form onSubmit={handleLogin}>
-        <Stack  spacing={3} alignItems="center">
-        <TextField required
-            id="outlined-required"
-            label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}/>
+    <div className={`AppLogin ${isLoggedIn ? "fade-out" : ""}`}>
+      <Stack spacing={5} alignItems="center">
+        <br />
+        <Typography variant="h4">Login Page</Typography>
+        <br />
+        <form className="loginform" onSubmit={handleLogin}>
+          <Stack spacing={3} alignItems="center">
+            <TextField className="usernamef"
+              required
+              id="outlined-required"
+              label="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
 
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Stack
-          direction="row"
-          spacing={8}
-          >
-            <Button onClick={handleGuestClick}>Guest</Button>
-             <Button variant="contained" type="submit">
-            Login
-          </Button>
+            <TextField className="passwordf"
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Stack direction="row" spacing={8}>
+              <Button className="guestbtn"  onClick={handleGuestClick}>Guest</Button>
+              <Button className="loginbtn" variant="contained" type="submit">
+                Login
+              </Button>
+            </Stack>
           </Stack>
-         
-        {/* <button type="submit">Login</button> */}
-        </Stack>
+        </form>
 
-      </form>
-
-      <p>{message}</p>
-       </Stack>
-         
+        <p>{message}</p>
+      </Stack>
     </div>
   );
 }

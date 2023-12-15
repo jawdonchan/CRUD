813d69fd53c46b7ec344cb00aa5f65f0e048a9bb
jawdonchan <118231622+jawdonchan@users.noreply.github.com>
2023-12-15 12:00:45 +0800

    Update email.jsx
    email css changes

--- client/src/pages/email/email.jsx
+++ client/src/pages/email/email.jsx
@@ -25,16 +25,56 @@ const EmailForm = () => {
   };
 
   return (
-    <div>
-      <Navbar></Navbar>
-      <label>Emails (comma-separated):</label>
-      <input type="text" value={emails} onChange={(e) => setEmails(e.target.value)} />
-      <label>Subject:</label>
-      <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
-      <label>Message:</label>
-      <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
-      <button onClick={handleSendEmails}>Send Emails</button>
-    </div>
+    <div className='emails'>
+  {/* <Navbar></Navbar> */}
+  <label className='labelmates'>Emails (comma-separated):</label>
+  <input type="text" value={emails} onChange={(e) => setEmails(e.target.value)} />
+  <label className='labelmates'>Subject:</label>
+  <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
+  <label className='labelmates'>Message:</label>
+  <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+  <button className='submittion' onClick={handleSendEmails}>Send Emails</button>
+  <style>
+    {`
+      .emails {
+        max-width: 300px; /* Set your preferred max-width */
+        margin: auto; /* Center the form horizontally */
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        //box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+
+      .labelmates {
+        display: block;
+        margin-bottom: 8px;
+      }
+
+      input,
+      textarea {
+        width: 100%;
+        padding: 8px;
+        margin-bottom: 16px;
+        box-sizing: border-box;
+      }
+
+      .submittion {
+        width: 100%;
+        padding: 10px;
+        background-color: #4caf50;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+
+      .submittion:hover {
+        background-color: #45a049;
+      }
+    `}
+  </style>
+</div>
+
   );
 };
 
